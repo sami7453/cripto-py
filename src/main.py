ea29528220942b699ea4dee6ff3f262f2b2bc9a7
PilotAlex <66020831+AlexandreAero@users.noreply.github.com>
2023-10-19 13:58:55 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,16 @@
-
-from affine import decryption_affine, encryption_affine
-from cesar import chiffrement_cesar, dechiffrement_cesar, craquage
-from perso import chiffrement_personnalise, dechiffrement_personnalise
-from substitution import chiffrement_substitution, dechiffrement_substitution
+from affine import encryption_affine, decryption_affine
+from cesar import encryption_cesar, decryption_cesar, craquage
+from substitution import encryption_substitution, decryption_substitution
+from perso import encryption_perso, decryption_perso
 
 def main_program():
-    print("Choisissez un algorithme ou une methode:")
+    print("Choisissez un algorithme:")
     print("1. César")
     print("2. Affine")
     print("3. Substitution")
-    print("4. craquage césar")
-    print("5. chiffrement libre")
+    print("4. Craquage César")
+    print("5. Chiffrement libre")
+
     choice = input("Entrez le numéro de votre choix: ")
 
     if choice == "1":
@@ -26,44 +26,41 @@ def main_program():
     elif choice == "5":
         key5 = int(input("Entrez la clé pour l'algorithme libre: "))
         key6 = int(input("Entrez le patern pour l'algorithme libre: "))
+    else:
+        raise IndexError()
 
-
-
-    if choice in ["1", "2","5"]:
+    if choice in ["1", "2", "5"]:
         action = input("Souhaitez-vous chiffrer ou déchiffrer? (c/d): ")
-    elif choice in ["4"]:
+    elif choice == "4":
         action = input("continuer (o/n): ")
     else:
         action = "c"
 
     text = input("Entrez le texte: ")
-
+   
     try:
         if choice == "1":
             if action == "c":
-                result = chiffrement_cesar(text, key)
+                result = encryption_cesar(text, key)
             else:
-                result = dechiffrement_cesar(text,key)
-            pass
+                result = decryption_cesar(text, key)
         elif choice == "2":
             if action == "c":
                 result = encryption_affine(text, key1, key2)
             else:
                 result = decryption_affine(text, key1, key2)
         elif choice == "3":
-            # Ici, vous devez ajouter la fonction de chiffrement de substitution si vous l'avez.
-            pass
+            if action == "c":
+                list, dict = encryption_substitution(text)
+                result = list
         elif choice == "4":
             if action == "o":
                 result = craquage(key3,key4)
-            else:
-                pass
         elif choice == "5":
             if action == "c":
-                result = chiffrement_personnalise(text, key5, key6)
+                result = encryption_perso(text, key5, key6)
             else:
-                result = dechiffrement_personnalise(text, key5, key6)
-
+                result = decryption_perso(text, key5, key6)
 
         print(f"Résultat: {result}")
 
@@ -71,6 +68,7 @@ def main_program():
         print(f"Erreur: {e}")
 
     again = input("Souhaitez-vous exécuter le programme à nouveau? (o/n): ")
+
     if again.lower() == "o":
         main_program()
 
