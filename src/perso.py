27f6510f8703f854eff0b72c9dce8535c49d0569
sami7453 <145992280+sami7453@users.noreply.github.com>
2023-10-19 13:35:49 +0200

    Update perso.py

--- src/perso.py
+++ src/perso.py
@@ -1,26 +1,72 @@
-def chiffrement_personnalise(texte):
-    cle = 5
+# Chiffrement qui prend en compte une chaine de caractère, une cle et un paterne. La cle est le décalage et le paterne
+# est au bout de combien de caractère on change le sens du décalage en passant du + au - et du - au +...
+
+def chiffrement_personnalise(texte, cle, paterne):
     texte_chiffre = ""
-    for caractere in texte:
-        if caractere.isalpha():
-            decale = ord(caractere) + cle
-            if caractere.islower():
-                if decale > ord('z'):
-                    decale -= 26
-                elif decale < ord('a'):
-                    decale += 26
-            elif caractere.isupper():
-                if decale > ord('Z'):
-                    decale -= 26
-                elif decale < ord('A'):
-                    decale += 26
-            caractere_chiffre = chr(decale)
-            texte_chiffre += caractere_chiffre
+    decalage = 0
+    changer_de_sens = False
+
+    for i in range(len(texte)):
+        caractere = texte[i]
+        if 'a' <= caractere <= 'z':
+            if changer_de_sens:
+                decalage = -cle
+            else:
+                decalage = cle
+            caractere_chiffre = chr(((ord(caractere) - ord('a') + decalage) % 26) + ord('a'))
+        elif 'A' <= caractere <= 'Z':
+            if changer_de_sens:
+                decalage = -cle
+            else:
+                decalage = cle
+            caractere_chiffre = chr(((ord(caractere) - ord('A') + decalage) % 26) + ord('A'))
         else:
-            texte_chiffre += caractere
+            caractere_chiffre = caractere
+
+        texte_chiffre += caractere_chiffre
+
+        if (ord(caractere) >= ord('a') and ord(caractere) <= ord('z')) or (ord(caractere) >= ord('A') and ord(caractere) <= ord('Z')):
+            if (i + 1) % paterne == 0:
+                changer_de_sens = not changer_de_sens
     print("Chaîne chiffrée :", texte_chiffre)
     return texte_chiffre
 
-texte_clair = "Ceci est un exemple de chiffrement simple."
-chiffrement_personnalise(texte_clair)
+
+
+
+# Déchiffrement prenant en compte une chaine de caractère, une cle et un paterne. Il s'agit d'une fonction qui déchiffre
+# la précédente. Alors si vous souhaiter déchiffrer le texte de la fonction précédente, il faut que la cle et le paterne
+# des 2 fonctions soient identiques.
+
+
+def dechiffrement_personnalise(texte_chiffre, cle, paterne):
+    texte_dechiffre = ""
+    decalage = 0
+    changer_de_sens = False
+
+    for i in range(len(texte_chiffre)):
+        caractere_chiffre = texte_chiffre[i]
+        if 'a' <= caractere_chiffre <= 'z':
+            if changer_de_sens:
+                decalage = -cle
+            else:
+                decalage = cle
+            caractere_dechiffre = chr(((ord(caractere_chiffre) - ord('a') - decalage) % 26) + ord('a'))
+        elif 'A' <= caractere_chiffre <= 'Z':
+            if changer_de_sens:
+                decalage = -cle
+            else:
+                decalage = cle
+            caractere_dechiffre = chr(((ord(caractere_chiffre) - ord('A') - decalage) % 26) + ord('A'))
+        else:
+            caractere_dechiffre = caractere_chiffre
+
+        texte_dechiffre += caractere_dechiffre
+
+        if (ord(caractere_chiffre) >= ord('a') and ord(caractere_chiffre) <= ord('z')) or (ord(caractere_chiffre) >= ord('A') and ord(caractere_chiffre) <= ord('Z')):
+            if (i + 1) % paterne == 0:
+                changer_de_sens = not changer_de_sens
+    print("Chaîne déchiffrée :", texte_dechiffre)
+    return texte_dechiffre
+
 
